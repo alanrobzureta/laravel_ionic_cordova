99557312fb4955ac16de107153ee7f483bb6a727
alanrobzureta <alanrobzureta@gmail.com>
2016-04-20 16:35:10 -0300

    Relatório Teto Orçamentário - Inclusão de Teto Fundo Fiocruz ao relatório Geral de Teto Orçamentário

---
+++ app/Cupom.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cupom extends Model
+{
+    protected $fillable = [
+        'name',
+        'cupom' 
+    ];
+    //
+}
---
+++ app/Http/Controllers/CupomController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cupom;
+use Illuminate\Http\Request;
+
+
+class CupomController extends Controller
+{
+    public function cupom($cupom)
+    {
+        $cupom = Cupom::where('cupom',$cupom)->first();
+        
+        $result = false;
+        if($cupom){
+            $result = true;
+        }        
+        
+        return response()->json(['result'=>$result]);
+    }
+    
+    public function store(Request $request)
+    {
+        $cupom = Cupom::create($request->all());
+        
+        if($cupom){
+            return response()->json($cupom);
+        } else {
+            return response()->json(['response'=>false]);
+        }
+    }
+}
